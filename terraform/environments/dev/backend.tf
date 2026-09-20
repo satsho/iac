a309d0de550ed40f93c2bd23bf08ddb0bfc71b33
satsho <emeth.meth0307@gmail.com>
2026-09-20 15:24:14 +0900

    Update backend.tf

--- terraform/environments/dev/backend.tf
+++ terraform/environments/dev/backend.tf
@@ -1,10 +1,9 @@
 terraform {
   backend "s3" {
-    # bootstrap の出力値に合わせて書き換える
     bucket       = "satsho-iac-tfstate"
     key          = "dev/vpc/terraform.tfstate"
     region       = "ap-northeast-1"
     encrypt      = true
-    use_lockfile = true # Terraform 1.10+ のS3ネイティブロック。DynamoDB不要
+    use_lockfile = true
   }
 }

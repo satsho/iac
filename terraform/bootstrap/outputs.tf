84a35f6b5de75912ead23e70e7b21a906f5ff365
satsho <emeth.meth0307@gmail.com>
2026-09-20 14:23:54 +0900

    Delete terraform directory

--- terraform/bootstrap/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "state_bucket_name" {
-  description = "environments/dev/backend.tf に設定するバケット名"
-  value       = aws_s3_bucket.tfstate.id
-}
-
-output "lock_table_name" {
-  description = "environments/dev/backend.tf に設定するDynamoDBテーブル名"
-  value       = aws_dynamodb_table.tfstate_lock.name
-}
